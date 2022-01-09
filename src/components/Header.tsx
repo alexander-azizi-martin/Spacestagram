e3490fabc43dc3540d8408905d81abfe1f62f0b1
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2022-01-09 15:28:13 -0500

    feat: :iphone: Made the header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { FiSearch } from 'react-icons/fi';
 import { useStore } from '~/utils/store';
 
 import SearchBar from '~/components/SearchBar';
 import DateRangePicker from '~/components/DateRangePicker';
 
 function Header() {
-  const setQuery = useStore((state) => state.updateSearchQuery);
-  const searchQuery = useStore((state) => state.searchQuery);
-
-  const [value, setValue] = useState(new Date());
-
   return (
-    <header className="flex items-center justify-between h-14 bg-white pr-[50px] pl-[50px]">
-      <div className="flex-1 md:flex-0"></div>
+    <header className="flex flex-wrap items-center justify-center flex-col sm:flex-row sm:justify-between gap-y-2 sm:gap-y-0 h-max py-3 sm:py-0 sm:h-14 bg-white px-2.5 md:px-2.5 sticky top-0">
+      <div className="flex-0 lg:flex-1"></div>
+
       <div className="flex-1">
         <SearchBar />
       </div>
+
       <div className="flex-1 flex justify-end">
         <DateRangePicker />
       </div>
