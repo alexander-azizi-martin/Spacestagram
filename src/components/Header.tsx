3baa5ab8a35a33f7582f6ad12020ddcf6b25f0e9
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2022-01-11 23:10:41 -0500

    fix: :iphone: Made the header more responsive for smaller screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,17 +8,17 @@ import DateRangePicker from '~/components/DateRangePicker';
 
 function Header() {
   return (
-    <header className="flex flex-wrap items-center justify-center flex-col sm:flex-row sm:justify-between gap-y-2 sm:gap-y-0 h-max py-3 sm:py-0 sm:h-14 bg-white px-2.5 md:px-14 sticky top-0">
-      <div className="flex-0 lg:flex-1 flex gap-x-10">
-        <div className='w-20'>
+    <header className="flex flex-wrap items-center justify-center flex-col md:flex-row sm:justify-between gap-y-2 md:gap-y-0 h-max py-3 md:py-0 md:h-14 bg-white px-2.5 gap-x-2 lg:px-14 sticky top-0">
+      <div className="flex-1 flex items-center justify-start gap-x-10">
+        <div className="w-20">
           <FilterDropDown />
         </div>
-        <div className='w-20'>
+        <div className="w-20">
           <SortDropDown />
         </div>
       </div>
 
-      <div className="flex-1">
+      <div className="flex-1 max-w-[500px]">
         <SearchBar />
       </div>
 
