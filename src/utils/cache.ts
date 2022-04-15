f1b6f9e156e1abae2c759b556e5fff0a36471d2e
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2022-04-15 17:45:46 -0400

    fix: :bug: Fix issue with deleting and clearing entries in cache

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,17 +1,22 @@
 type Entry<KeyT, DataT> = {
-  key: KeyT | null;
-  data: DataT | null;
+  key: KeyT;
+  data: DataT;
+  prev: Entry<KeyT, DataT> | null;
   next: Entry<KeyT, DataT> | null;
 };
 
 class Cache<KeyT, DataT> {
   max_size: number;
-  entries: Map<KeyT, DataT>;
+  entries: Map<KeyT, Entry<KeyT, DataT>>;
 
   head: Entry<KeyT, DataT> | null;
   tail: Entry<KeyT, DataT> | null;
 
   constructor(max_size: number) {
+    if (max_size <= 0) {
+      throw new Error('Size must be greater than 0.');
+    }
+
     this.max_size = max_size;
     this.entries = new Map();
 
@@ -20,7 +25,7 @@ class Cache<KeyT, DataT> {
   }
 
   get(key: KeyT) {
-    return this.entries.get(key);
+    return this.entries.get(key)?.data;
   }
 
   has(key: KeyT) {
@@ -28,30 +33,43 @@ class Cache<KeyT, DataT> {
   }
 
   set(key: KeyT, data: DataT) {
-    let entry = { key, data, next: null };
+    const entry = { key, data, prev: this.tail, next: null };
 
     if (this.head == null) {
       this.head = entry;
-    }
-    if (this.tail != null) {
+      this.tail = entry;
+    } else {
+      // @ts-ignore If the head is not null neither is the tail
       this.tail.next = entry;
+      this.tail = entry;
     }
 
     // Deletes the oldest element in the cash if it reaches it limit
     if (this.max_size <= this.entries.size && this.head) {
+      const newHead = this.head.next;
       this.delete(this.head.key as KeyT);
-      this.head = this.head.next;
+      this.head = newHead;
     }
 
-    this.tail = entry;
-    this.entries.set(key, data);
+    this.entries.set(key, entry);
   }
 
   delete(key: KeyT) {
+    const entry = this.entries.get(key);
+    if (entry && entry.prev) {
+      entry.prev.next = entry.next;
+    }
+    if (entry && entry.next) {
+      entry.next.prev = entry.prev;
+    }
+
     return this.entries.delete(key);
   }
 
   clear() {
+    this.head = null;
+    this.tail = null;
+
     return this.entries.clear();
   }
 }
