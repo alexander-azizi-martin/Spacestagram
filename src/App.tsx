2a1841bb12b32d76de62eff7671a5531eed632e1
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2022-04-15 21:12:32 -0400

    refactor: :recycle: Extract logic from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, forwardRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import escapeStringRegexp from 'escape-string-regexp';
 import { AxiosError } from 'axios';
 import dayjs, { Dayjs } from 'dayjs';
@@ -14,87 +14,43 @@ import { X as CloseIcon, Plus } from 'react-feather';
 import Header from '~/components/Header';
 import ApodList from '~/components/ApodList';
 
-type Alert = {
-  message: string;
-  severity: AlertColor;
+const FIRST_DAY = dayjs('1995-06-16', 'YYYY-MM-DD');
+const TODAY = dayjs().startOf('day');
+
+interface Errors {
+  [errorCode: number]: string;
+}
+
+const ERROR_MESSAGES: Errors = {
+  4: 'Something went wrong with Nasa servers while fetching api data try refreshing the page',
+  5: "Something went wrong while fetching data from Nasa's api try refreshing the page",
 };
 
-const firstDate = dayjs('1995-06-16', 'YYYY-MM-DD');
-const today = dayjs().startOf('day');
-
 function App() {
-  const [
-    searchQuery,
-    filterOption,
-    sortOption,
-    likedApods,
-    startDate,
-    endDate,
-    updateStartDate,
-    updateEndDate,
-  ] = useStore((state) => [
-    state.searchQuery,
-    state.filterOption,
-    state.sortOption,
-    state.likedApods,
-    state.startDate,
-    state.endDate,
-    state.updateStartDate,
-    state.updateEndDate,
-  ]);
+  const [sortOption, startDate, endDate, updateStartDate, updateEndDate] =
+    useStore((state) => [
+      state.sortOption,
+      state.startDate,
+      state.endDate,
+      state.updateStartDate,
+      state.updateEndDate,
+    ]);
 
-  const selectApods = (apods: ApodInfo[]) => {
-    return apods
-      .filter((apod) => {
-        if (filterOption == 'liked' && !likedApods.has(apod.date)) {
-          return false;
-        } else if (filterOption == 'unliked' && likedApods.has(apod.date)) {
-          return false;
-        } else if (
-          searchQuery &&
-          !apod.title.toLowerCase().match(escapeStringRegexp(searchQuery))
-        ) {
-          return false;
-        }
+  const [alert, setAlert] = useAlert();
+  const [apods, addApods, clearApods] = useFilteredApods();
 
-        return true;
-      })
-      .sort((apod1, apod2) => {
-        const apod1Date = dayjs(apod1.date, 'YYYY-MM-DD');
-        const apod2Date = dayjs(apod2.date, 'YYYY-MM-DD');
-
-        if (sortOption == 'newest') {
-          return apod1Date.isBefore(apod2Date) ? 1 : -1;
-        } else if (sortOption == 'oldest') {
-          return apod1Date.isBefore(apod2Date) ? -1 : 1;
-        }
-
-        return 0;
-      });
-  };
-
-  const removeDuplicates = (apods: ApodInfo[]) => {
-    const seen = new Set<string>();
-
-    return apods.filter((apod) => {
-      if (seen.has(apod.date)) {
-        return false;
-      }
-
-      seen.add(apod.date);
-      return true;
-    });
-  };
-
-  const [alert, setAlert] = useState<Alert>();
   const [cursor, setCursor] = useState(
     sortOption == 'newest' ? endDate : startDate,
   );
-  const [apods, setApods] = useState<ApodInfo[]>([]);
+  const errorOccurred = useRef(false);
+  const fetchingApods = useRef(false);
   const fetchNextApods = () => {
-    console.log('Fetching data');
-    let nextCursor: Dayjs;
+    if (fetchingApods.current) {
+      return;
+    }
+    fetchingApods.current = true;
 
+    let nextCursor: Dayjs;
     if (sortOption == 'newest') {
       nextCursor = cursor.subtract(5, 'days');
 
@@ -120,112 +76,70 @@ function App() {
 
     fetchApods(cursor, nextCursor)
       .then((newApods) => {
+        addApods(newApods);
+
+        if (sortOption == 'newest') {
+          nextCursor = nextCursor.subtract(1, 'day');
+        } else {
+          nextCursor = nextCursor.add(1, 'day');
+        }
+        setCursor(nextCursor);
+      })
+      .catch((error) => {
+        const errorType = Math.floor(error.response.status / 100);
+        if (errorType in ERROR_MESSAGES) {
+          setAlert({ message: ERROR_MESSAGES[errorType], severity: 'error' });
+        } else {
+          setAlert({ message: 'Unknown error occurred', severity: 'error' });
+        }
+
+        // Prevents more request being sent
+        errorOccurred.current = true;
+      })
+      .finally(() => {
         if (timeoutID) {
           clearTimeout(timeoutID);
         }
 
-        setApods(removeDuplicates(apods.concat(newApods)));
-        setCursor(nextCursor);
-      })
-      .catch((error) => {
-        const errorType = error.response.status / 100;
-
-        switch (errorType) {
-          case 5:
-            setAlert({
-              message:
-                'Something went wrong with Nasa servers while fetching api data try refreshing the page',
-              severity: 'error',
-            });
-            break;
-          case 4:
-            setAlert({
-              message:
-                "Something went wrong while fetching data from Nasa's api try refreshing the page",
-              severity: 'error',
-            });
-            break;
-          default:
-            setAlert({
-              message: 'Unknown error occurred',
-              severity: 'error',
-            });
-            break;
-        }
-
-        // Prevents more request being sent
-        if (sortOption == 'newest') {
-          updateStartDate(cursor);
-        } else if (sortOption == 'oldest') {
-          updateEndDate(cursor);
-        }
+        fetchingApods.current = false;
       });
   };
 
+  // Handles loading another month
   const [loadedMonth, setLoadedMonth] = useState(false);
   const loadMonth = () => {
     if (sortOption == 'newest') {
       let nextStartDate = startDate.subtract(1, 'month');
-      if (!nextStartDate.isAfter(firstDate)) {
-        nextStartDate = firstDate;
+
+      // Rounds to the first date that APOD started
+      if (!nextStartDate.isAfter(FIRST_DAY)) {
+        nextStartDate = FIRST_DAY;
       }
 
       updateStartDate(nextStartDate);
     } else if (sortOption == 'oldest') {
       let nextEndDate = endDate.add(1, 'month');
-      if (!nextEndDate.isBefore(today)) {
-        nextEndDate = today;
+
+      // Rounds to today since you cant see APODS of the future
+      if (!nextEndDate.isBefore(TODAY)) {
+        nextEndDate = TODAY;
       }
 
       updateEndDate(nextEndDate);
     }
 
+    // Prevents whole screen from refreshing
     setLoadedMonth(true);
   };
-
-  let showLoadMonth: boolean;
-  if (sortOption == 'newest') {
-    showLoadMonth = !startDate.isSame(firstDate, 'day');
-  } else {
-    showLoadMonth = !endDate.isSame(today, 'day');
-  }
-
-  useEffect(() => {
-    if (alert) {
-      let timeoutID: NodeJS.Timeout | null = setTimeout(() => {
-        timeoutID = null;
-        setAlert(undefined);
-      }, 10000);
-
-      return () => {
-        if (timeoutID) {
-          clearTimeout(timeoutID);
-        }
-      };
-    }
-  }, [alert]);
 
   useEffect(() => {
     if (!loadedMonth) {
       setCursor(sortOption == 'newest' ? endDate : startDate);
-      setApods([]);
+      clearApods();
     } else {
       setLoadedMonth(false);
     }
   }, [startDate, endDate, sortOption]);
-
-  const hasMore = () => {
-    const numApods =
-      Math.round((endDate.unix() - startDate.unix()) / SECONDS_IN_DAY) + 1;
-    let hasMore: boolean;
-    if (sortOption == 'newest') {
-      hasMore = !cursor.isSame(startDate, 'day') || 1 > apods.length;
-    } else {
-      hasMore = !cursor.isSame(endDate, 'day') || 1 > apods.length;
-    }
-
-    return hasMore;
-  };
 
   useEffect(() => {
     // InfiniteScroll component won't trigger without a scroll event
@@ -234,8 +148,26 @@ function App() {
       window.dispatchEvent(new CustomEvent('scroll'));
     }
   });
-  
-  const selectedApods = selectApods(apods);
+
+  const hasMore = () => {
+    let hasMore: boolean;
+    if (sortOption == 'newest') {
+      hasMore =
+        cursor.isAfter(startDate, 'day') || cursor.isSame(startDate, 'day');
+    } else {
+      hasMore =
+        cursor.isBefore(endDate, 'day') || cursor.isSame(endDate, 'day');
+    }
+
+    return hasMore && !errorOccurred.current;
+  };
+
+  let showLoadMonth: boolean;
+  if (sortOption == 'newest') {
+    showLoadMonth = !startDate.isSame(FIRST_DAY, 'day');
+  } else {
+    showLoadMonth = !endDate.isSame(TODAY, 'day');
+  }
 
   return (
     <>
@@ -246,7 +178,6 @@ function App() {
           dataLength={Math.random()}
           next={fetchNextApods}
           hasMore={hasMore()}
-          scrollThreshold="0px"
           endMessage={
             showLoadMonth ? (
               <Button
@@ -262,23 +193,9 @@ function App() {
           }
           loader={<CircularProgress />}
         >
-          <ApodList apodList={selectedApods} />
+          <ApodList apodList={apods} />
         </InfiniteScroll>
-        {selectedApods.length > 1 && (
-          <div className="hidden lg:block flex-0 sticky top-0 h-[100vh] self-start">
-            <div className="sticky top-[calc(100vh-60px)] w-max">
-              <div className="relative left-10">
-                <Button
-                  onClick={() => {
-                    window.scrollTo({ top: 0, behavior: 'smooth' });
-                  }}
-                >
-                  Back to Top
-                </Button>
-              </div>
-            </div>
-          </div>
-        )}
+        {apods.length > 1 && <BackToTopButton />}
         {alert != undefined && (
           <Slide className="absolute bottom-10" direction="up" in={true}>
             <Alert
@@ -295,6 +212,113 @@ function App() {
       </main>
     </>
   );
+}
+
+function BackToTopButton() {
+  return (
+    <div className="hidden lg:block flex-0 sticky top-0 h-[100vh] self-start">
+      <div className="relative sticky top-[calc(100vh-60px)]">
+        <div className="absolute left-10 w-max">
+          <Button
+            onClick={() => {
+              window.scrollTo({ top: 0, behavior: 'smooth' });
+            }}
+          >
+            Back to Top
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+type Alert = {
+  message: string;
+  severity: AlertColor;
+};
+
+type UseAlert = [Alert | undefined, (alert: Alert | undefined) => void];
+
+function useAlert(): UseAlert {
+  const [alert, setAlert] = useState<Alert>();
+
+  useEffect(() => {
+    if (alert) {
+      let timeoutID: NodeJS.Timeout | null = setTimeout(() => {
+        timeoutID = null;
+        setAlert(undefined);
+      }, 10000);
+
+      return () => {
+        if (timeoutID) {
+          clearTimeout(timeoutID);
+        }
+      };
+    }
+  }, [alert]);
+
+  return [alert, setAlert];
+}
+
+type UseFilteredApods = [ApodInfo[], (apods: ApodInfo[]) => void, () => void];
+
+function useFilteredApods(): UseFilteredApods {
+  const [searchQuery, filterOption, sortOption, likedApods] = useStore(
+    (state) => [
+      state.searchQuery,
+      state.filterOption,
+      state.sortOption,
+      state.likedApods,
+    ],
+  );
+
+  const [filteredApods, setFilteredApods] = useState<ApodInfo[]>([]);
+  const apods = useRef<ApodInfo[]>([]);
+
+  const addApods = (newApods: ApodInfo[]) => {
+    apods.current = apods.current.concat(newApods).sort((apod1, apod2) => {
+      const apod1Date = dayjs(apod1.date, 'YYYY-MM-DD');
+      const apod2Date = dayjs(apod2.date, 'YYYY-MM-DD');
+
+      if (sortOption == 'newest') {
+        return apod1Date.isBefore(apod2Date) ? 1 : -1;
+      } else if (sortOption == 'oldest') {
+        return apod1Date.isBefore(apod2Date) ? -1 : 1;
+      }
+
+      return 0;
+    });
+  };
+
+  const clearApods = () => {
+    setFilteredApods([]);
+    apods.current = [];
+  };
+
+  useEffect(() => {
+    setFilteredApods(
+      apods.current.filter((apod) => {
+        switch (filterOption) {
+          case 'liked':
+            if (!likedApods.has(apod.date)) return false;
+            break;
+          case 'unliked':
+            if (likedApods.has(apod.date)) return false;
+            break;
+        }
+
+        if (searchQuery) {
+          return apod.title
+            .toLowerCase()
+            .match(escapeStringRegexp(searchQuery));
+        }
+
+        return true;
+      }),
+    );
+  }, [apods.current, searchQuery, filterOption, sortOption, likedApods]);
+
+  return [filteredApods, addApods, clearApods];
 }
 
 export default App;
