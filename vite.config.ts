80ee18d74d10b2dd22ba7fa9411d4a5b62f41175
alexander-azizi-martin <alexander.azizi.martin@gmail.com>
2022-04-15 21:11:51 -0400

    refactor: :truck: Move static files into src directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
   resolve: {
     alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
   },
